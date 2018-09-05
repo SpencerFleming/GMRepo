832f95297b40252c6767ef934d83f0a796d241be
Spencer Fleming <spencerfleming@u.boisestate.edu>
2018-09-05 15:24:12 -0600

    Initial commit

---
+++ Dao.php
@@ -0,0 +1,13 @@
+<?php
+class dao {
+	private $host = "us-cbdr-iron-east-01.cleardb.net";
+	private $db   = "heroku_ad951a78739c51c";
+	private $user = "bb8f5bb40607a5";
+	private $pass = "45bea5ad";
+	public function getConnection () {
+		return
+			new PDO("mysql:host={$this->host}
+			;dbname={$this->db}", {$this->user}, 
+			{$this->pass});
+	}
+}
