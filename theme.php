0139b27c627593c3b50a92c079c36164b6d72b36
SpencerFleming <spencerfleming@u.boisestate.edu>
2018-09-12 19:08:54 -0600

    turned css file into php

---
+++ theme.php
@@ -0,0 +1,17 @@
+<?php
+	header("Content-type: text/css");
+	$sitebg = '#1a1a1a';
+	$mainbg = '#777777';
+	$tbarbg = '#373737';
+?>
+
+.topbarbutton {
+	background-color: <?=$tbarbg?>;
+	border: none;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+}
