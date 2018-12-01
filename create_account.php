e9f9e9bafeea4b1e84b83f957f4b298a2e052493
SpencerFleming <spencerfleming@u.boisestate.edu>
2018-11-30 19:02:02 -0700

    Begins implementing better errors for account creation

--- create_account.php
+++ create_account.php
@@ -16,52 +16,60 @@ function failAccount($status) {
 
 $dao = new Dao();
 
+$status = "";
+
 // Fail if email is invalid
 if ($_POST["email"] == "") {
-    failAccount("Please enter email address");
+    $status = $status . "ERROR: No email address\n";
 }
-if ($_POST["email"] != filter_var($_POST["email"], FILTER_SANITIZE_EMAIL)) {
-    failAccount("Email contains invalid characters");
+else if ($_POST["email"] != filter_var($_POST["email"], FILTER_SANITIZE_EMAIL)) {
+    $status = $status . "ERROR: Email contains invalid characters\n";
 }
-if ($_POST["email"] != filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    failAccount("Invalid email address");
+else if ($_POST["email"] != filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    $status = $status . "ERROR: Invalid email address\n";
 }
-
-// Fail if email already exists in database
-$testpass = $dao->getPassword($_POST["email"]);
-if ($testpass != null) {
-    failAccount("Account with this email address already exists");
+else {
+    // Fail if email already exists in database
+    $testpass = $dao->getPassword($_POST["email"]);
+    if ($testpass != null) {
+        $status = $status . "ERROR: Account with this email address already exists\n";
+    }
 }
 
 // Fail if username fails regex comparison
-$usernameRegex = "^(?=.{8,20}$)[a-zA-Z0-9._]^";
+$usernameRegex = "^(?=.{5,20}$)[a-zA-Z0-9._]$";
 $isUsernameValid = preg_match($usernameRegex, $_POST["username"]);
 if ($isUsernameValid === 0 || $isUsernameValid === false) {
-    failAccount(
-        "Username must be 8-20 characters long, and only contain alphanumeric characters," .
-        " underscore and dot"
-    );
+    $status = $status .
+        "ERROR: Username must be 5-20 characters long, and only contain alphanumeric characters," .
+        " underscore and dot\n";
 }
+else {
 
 // EXAMPLE OF SANITATION: Make sure username doesn't contain the word 'poo'
-if (strpos($_POST["username"], "poo") !== false) {
-    failAccount("Username cannot contain the word 'poo' you poor troglodyte");
-}
+//if (strpos($_POST["username"], "poo") !== false) {
+//    failAccount("Username cannot contain the word 'poo' you poor troglodyte");
+//}
 
-// Fail is username already exists in database
-$testuser = $dao->checkUsername($_POST["username"]);
-if ($testuser) {
-    failAccount("Username already exists.");
+    // Fail is username already exists in database
+    $testuser = $dao->checkUsername($_POST["username"]);
+    if ($testuser) {
+        $status = $status . "ERROR: Username already exists\n";
+    }
 }
 
 // Make sure that password is at least 8 characters long.
 if (strlen($_POST["password"]) < 8) {
-    failAccount("Password must contain at least 8 characters");
+    $status = $status . "ERROR: Password must contain at least 8 characters\n";
 }
 
 // Make sure that password re-entry matches password.
 if ($_POST["password"] != $_POST["passwordconf"]) {
-    failAccount("Password re-entry must match password");
+    $status = $status . "ERROR: Password re-entry must match password\n";
+}
+
+if ($status != "") {
+    failAccount($status);
 }
 
 // Pass account if there are no problems.
